332c6942787d4a7add1af9fea3b3a495d8987b82
danielforeverlearning <danielforeverlearning@gmail.com>
2015-04-23 18:50:52 -1000

    1st check in
    from table VarDef to C# enums

---
+++ VarDef_To_Enum/EnumCollect.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarDef_To_Enum
+{
+    public class EnumCollect
+    {
+        private string Name;
+        List<string> Values;
+
+        private EnumCollect()
+        {
+
+        }
+        public EnumCollect(string _name)
+        {
+            Name = _name;
+            Values = new List<string>();
+        }
+
+        public void AddValue(string _value)
+        {
+            this.Values.Add(_value);
+        }
+
+        public void PrintMe()
+        {
+            
+            Console.WriteLine();
+            Console.WriteLine(string.Format("public enum {0}", this.Name));
+            Console.WriteLine("{");
+            int end = Values.Count - 1;
+            for (int ii = 0; ii < end; ii++)
+            {
+                Console.WriteLine(string.Format("    {0},", Values[ii]));
+            }
+            Console.WriteLine(string.Format("    {0}", Values[end]));
+            Console.WriteLine("};");
+        }
+
+
+    }//class
+}//namespace
---
+++ VarDef_To_Enum/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace VarDef_To_Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connstr = "data source=STEVESPC;initial catalog=RPAD;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
+            string querystr = "SELECT * FROM [RPAD].[dbo].[VarDef];";
+            List<string> myNames = new List<string>();
+            List<EnumCollect> myEnums = new List<EnumCollect>();
+            using (SqlConnection conn = new SqlConnection(connstr))
+            {
+                SqlCommand cmd = new SqlCommand(querystr, conn);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                try
+                {
+                    EnumCollect temp = null;
+                    while (reader.Read())
+                    {
+                        string mystr = string.Format("{0}, {1}", reader["VarGroupName"], reader["VarValue"]);
+                        if (myNames.Contains(reader["VarGroupName"]) == false)
+                        {
+                            myNames.Add(reader["VarGroupName"].ToString());
+                            //save old one if any
+                            if (temp != null)
+                                myEnums.Add(temp);
+                            //make new one
+                            temp = new EnumCollect(reader["VarGroupName"].ToString());
+                        }
+                        temp.AddValue(reader["VarValue"].ToString());
+                    }
+                    //save very last one
+                    myEnums.Add(temp);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    reader.Close();
+                }
+
+                foreach (EnumCollect ee in myEnums)
+                {
+                    ee.PrintMe();
+                }
+            }
+        }
+    }//class
+}//namespace
